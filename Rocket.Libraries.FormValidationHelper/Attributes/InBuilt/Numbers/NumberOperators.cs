69c222abd88288696405542a340c5a922d0f095d
nyingi <nyingi.maina@gmail.com>
2020-05-14 21:10:36 +0300

    Added validator for max numbers

--- Rocket.Libraries.FormValidationHelper/Attributes/InBuilt/Numbers/NumberOperators.cs
+++ Rocket.Libraries.FormValidationHelper/Attributes/InBuilt/Numbers/NumberOperators.cs
@@ -3,6 +3,8 @@ namespace Rocket.Libraries.FormValidationHelper.Attributes.InBuilt.Numbers
     internal static class NumberOperators
     {
         public const string GreaterThan = "GreaterThan";
+
+        public const string LessThan = "LessThan";
         public const string EqualTo = "EqualTo";
     }
 }
