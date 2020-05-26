1b16e9933a6d5c1cb3b96209ca73dab8ede34b2d
nyingi <nyingi.maina@gmail.com>
2020-05-26 11:38:26 +0300

    Removed unused variables and overrode the 'ToString' method of the ValidationError class to show the error Key where available

--- Rocket.Libraries.FormValidationHelper/ValidationError.cs
+++ Rocket.Libraries.FormValidationHelper/ValidationError.cs
@@ -16,5 +16,21 @@ namespace Rocket.Libraries.FormValidationHelper
         /// Gets or sets a list of errors that occured when field identified by <see cref="Key"/>
         /// </summary>
         public ImmutableList<string> Errors { get; set; }
+
+        /// <summary>
+        /// If available returns the key of the error.
+        /// </summary>
+        /// <returns>String with the error key where available.</returns>
+        public override string ToString ()
+        {
+            if (!string.IsNullOrEmpty (Key))
+            {
+                return Key;
+            }
+            else
+            {
+                return base.ToString ();
+            }
+        }
     }
 }
