6dd7c1f3d4b8bf6288f515af05dbea454ad634ea
nyingi <nyingi.maina@gmail.com>
2020-07-18 06:52:39 +0300

    Added tests

--- Rocket.Libraries.FormValidationHelperTests/ValidationResponseHelperTests.cs
+++ Rocket.Libraries.FormValidationHelperTests/ValidationResponseHelperTests.cs
@@ -10,26 +10,61 @@ namespace Rocket.Libraries.FormValidationHelperTests
     public class ValidationResponseHelperTests
     {
         [Fact]
-        public void SuccessReturnerWorksCorrectly()
+        public void GenericSuccessReturnWorksCorrectly ()
         {
-            const byte expectedResult = 235;
-            var result = new ValidationResponseHelper().SuccessValue(expectedResult);
-            Assert.Equal(expectedResult, result.Entity);
+            var result = new ValidationResponseHelper ().Success ();
+            Assert.Null (result.Entity);
+            Assert.Null (result.ValidationErrors);
         }
 
+        [Fact]
+        public async Task SuccessValueAsyncWorksCorrectly ()
+        {
+            const byte entityValue = 9;
+            var result = await new ValidationResponseHelper ().SuccessValueAsync (entityValue);
+            Assert.Equal (entityValue, result.Entity);
+            Assert.Null (result.ValidationErrors);
+        }
 
         [Fact]
-        public void ErrorReturnerWorksCorrectly()
+        public async Task RouteResponseAsyncWithTypeWorks ()
+        {
+            const int entityValue = 3;
+            const int doubledValue = entityValue * 2;
+
+            var validationResponse = new ValidationResponse<int>
+            {
+                Entity = entityValue
+            };
+
+            Func<int, Task<ValidationResponse<int>>> doubler = async (v) => await new ValidationResponseHelper ().SuccessValueAsync (v * 2);
+
+            var result = await new ValidationResponseHelper ().RouteResponseAsync (validationResponse, doubler);
+
+            Assert.Equal (doubledValue, result.Entity);
+            Assert.Null (result.ValidationErrors);
+        }
+
+        [Fact]
+        public void SuccessReturnerWorksCorrectly ()
+        {
+            const byte expectedResult = 235;
+            var result = new ValidationResponseHelper ().SuccessValue (expectedResult);
+            Assert.Equal (expectedResult, result.Entity);
+        }
+
+        [Fact]
+        public void ErrorReturnerWorksCorrectly ()
         {
             const string expectedResult = "  This is an error message with      lots of spaces and stuff ";
             const string key = "XUnitTestErrorKey";
-            var result = new ValidationResponseHelper().TypedError<int>(expectedResult,key);
-            var actualError = result.ValidationErrors.Single(a => a.Key.Equals(key, StringComparison.InvariantCulture));
-            Assert.Equal(expectedResult, actualError.Errors.Single());
+            var result = new ValidationResponseHelper ().TypedError<int> (expectedResult, key);
+            var actualError = result.ValidationErrors.Single (a => a.Key.Equals (key, StringComparison.InvariantCulture));
+            Assert.Equal (expectedResult, actualError.Errors.Single ());
         }
 
         [Fact]
-        public void TranslationWorksCorrectly()
+        public void TranslationWorksCorrectly ()
         {
             const byte value = 235;
             var initialResponse = new ValidationResponse<byte>
@@ -37,29 +72,29 @@ namespace Rocket.Libraries.FormValidationHelperTests
                 Entity = value
             };
 
-            var translatedResult = new ValidationResponseHelper().Translate<byte, int>(initialResponse);
+            var translatedResult = new ValidationResponseHelper ().Translate<byte, int> (initialResponse);
 
-            Assert.Equal(typeof(int), translatedResult.Entity.GetType());
+            Assert.Equal (typeof (int), translatedResult.Entity.GetType ());
         }
 
         [Theory]
-        [InlineData(true)]
-        [InlineData(false)]
-        public async Task RoutingTypedWhenInErrorWorksCorrectly(bool hasError)
+        [InlineData (true)]
+        [InlineData (false)]
+        public async Task RoutingTypedWhenInErrorWorksCorrectly (bool hasError)
         {
             const string errorText = "This is the error text";
             const string errorKey = "XUnitErrorKey";
             const string successText = "There Was No Error";
 
-            Func<string, Task<ValidationResponse<string>>> validDataCallback = (s) => Task.Run(() =>  new ValidationResponse<string>
+            Func<string, Task<ValidationResponse<string>>> validDataCallback = (s) => Task.Run (() => new ValidationResponse<string>
             {
                 Entity = s
             });
 
-            var validationErrors = ImmutableList<ValidationError>.Empty.Add(new ValidationError
+            var validationErrors = ImmutableList<ValidationError>.Empty.Add (new ValidationError
             {
-                Errors = ImmutableList<string>.Empty.Add(errorText),
-                Key = errorKey
+                Errors = ImmutableList<string>.Empty.Add (errorText),
+                    Key = errorKey
             });
 
             var response = new ValidationResponse<string>
@@ -68,41 +103,40 @@ namespace Rocket.Libraries.FormValidationHelperTests
                 ValidationErrors = hasError ? validationErrors : default
             };
 
-            var result = await new ValidationResponseHelper().RouteResponseTypedAsync<string,string>(
-                response,
-                async (validString) => await validDataCallback(validString));
+            var result = await new ValidationResponseHelper ().RouteResponseTypedAsync<string, string> (
+                    response,
+                    async (validString) => await validDataCallback (validString));
 
-            if(hasError)
+            if (hasError)
             {
-                Assert.Equal(errorKey, result.ValidationErrors.Single().Key);
-                Assert.Equal(errorText, result.ValidationErrors.Single().Errors.Single());
+                Assert.Equal (errorKey, result.ValidationErrors.Single ().Key);
+                Assert.Equal (errorText, result.ValidationErrors.Single ().Errors.Single ());
             }
             else
             {
-                Assert.Equal(successText, result.Entity);
+                Assert.Equal (successText, result.Entity);
             }
 
         }
 
-
         [Theory]
-        [InlineData(true)]
-        [InlineData(false)]
-        public async Task RoutingBasicWorksCorrectly(bool hasError)
+        [InlineData (true)]
+        [InlineData (false)]
+        public async Task RoutingBasicWorksCorrectly (bool hasError)
         {
             const string errorText = "This is the error text";
             const string errorKey = "XUnitErrorKey";
             const string successText = "There Was No Error";
 
-            Func<string, Task<ValidationResponse<string>>> validDataCallback = (s) => Task.Run(() =>  new ValidationResponse<string>
+            Func<string, Task<ValidationResponse<string>>> validDataCallback = (s) => Task.Run (() => new ValidationResponse<string>
             {
                 Entity = s
             });
 
-            var validationErrors = ImmutableList<ValidationError>.Empty.Add(new ValidationError
+            var validationErrors = ImmutableList<ValidationError>.Empty.Add (new ValidationError
             {
-                Errors = ImmutableList<string>.Empty.Add(errorText),
-                Key = errorKey
+                Errors = ImmutableList<string>.Empty.Add (errorText),
+                    Key = errorKey
             });
 
             var response = new ValidationResponse<object>
@@ -110,18 +144,18 @@ namespace Rocket.Libraries.FormValidationHelperTests
                 ValidationErrors = hasError ? validationErrors : default
             };
 
-            var result = await new ValidationResponseHelper().RouteResponseAsync<string>(
+            var result = await new ValidationResponseHelper ().RouteResponseAsync<string> (
                 response,
-                async () => await validDataCallback(successText));
+                async () => await validDataCallback (successText));
 
-            if(hasError)
+            if (hasError)
             {
-                Assert.Equal(errorKey, result.ValidationErrors.Single().Key);
-                Assert.Equal(errorText, result.ValidationErrors.Single().Errors.Single());
+                Assert.Equal (errorKey, result.ValidationErrors.Single ().Key);
+                Assert.Equal (errorText, result.ValidationErrors.Single ().Errors.Single ());
             }
             else
             {
-                Assert.Equal(successText, result.Entity);
+                Assert.Equal (successText, result.Entity);
             }
 
         }
