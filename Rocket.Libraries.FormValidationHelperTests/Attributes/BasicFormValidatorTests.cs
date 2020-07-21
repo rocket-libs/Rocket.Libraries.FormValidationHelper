5f66b0240c960a676967f18d6a9a6e0294f2b035
nyingi <nyingi.maina@gmail.com>
2020-07-21 09:02:34 +0300

    Added tests for ValidateAndPackAsync

---
+++ Rocket.Libraries.FormValidationHelperTests/Attributes/BasicFormValidatorTests.cs
@@ -0,0 +1,57 @@
+using System.Linq;
+using System.Threading.Tasks;
+using Rocket.Libraries.FormValidationHelper.Attributes;
+using Rocket.Libraries.FormValidationHelper.Attributes.InBuilt.Numbers;
+using Xunit;
+
+namespace Rocket.Libraries.FormValidationHelperTests.Attributes
+{
+    public class BasicFormValidatorTests
+    {
+        public const int MinimumId = 1;
+        
+
+        [Fact]
+        public async Task ValidateAndPackReturnsErrorsWhenTheyOccur()
+        {
+            var model = new Model();
+            using(var validator = new BasicFormValidator<Model>())
+            {
+                var result = await validator.ValidateAndPackAsync(model);
+                Assert.Null(result.Entity);
+                Assert.Equal(
+                    new MinimumNumberAttribute(MinimumId).ErrorMessage,
+                    result.ValidationErrors.Single().Errors.Single());
+                Assert.Equal(
+                    $"{nameof(Model)}.{nameof(Model.Id)}",
+                    result.ValidationErrors.Single().Key
+                );
+            }
+        }
+
+        [Fact]
+        public async Task ValidateAndPackReturnsSuccessObjectWhenThereAreNoErrors()
+        {
+            const int Id = 7;
+            var model = new Model
+            {
+                Id = Id
+            };
+            using(var validator = new BasicFormValidator<Model>())
+            {
+                var result = await validator.ValidateAndPackAsync(model);
+                Assert.Null(result.ValidationErrors);
+                Assert.Equal(
+                   Id,
+                   result.Entity.Id
+                );
+            }
+        }
+    }
+
+    class Model
+    {
+        [MinimumNumber(BasicFormValidatorTests.MinimumId)]
+        public int Id { get; set; }
+    }
+}
