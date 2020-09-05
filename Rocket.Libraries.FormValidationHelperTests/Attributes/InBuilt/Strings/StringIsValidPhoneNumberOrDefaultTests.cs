284de987ab17db410cb35397bf0b2ea096632a04
nyingi <nyingi.maina@gmail.com>
2020-09-05 11:05:56 +0300

    Added validation for phone numbers

---
+++ Rocket.Libraries.FormValidationHelperTests/Attributes/InBuilt/Strings/StringIsValidPhoneNumberOrDefaultTests.cs
@@ -0,0 +1,19 @@
+using Rocket.Libraries.FormValidationHelper.Attributes.InBuilt.Strings;
+using Xunit;
+
+namespace Rocket.Libraries.FormValidationHelperTests.Attributes.InBuilt.Strings
+{
+    public class StringIsValidPhoneNumberOrDefaultTests
+    {
+        [Theory]
+        [InlineData ("+254721111111")]
+        [InlineData ("+254 721 1 1 1 111")]
+        [InlineData("")]
+        [InlineData(null)]
+        public void NumberWithCountryCodePasses (string phoneNumber)
+        {
+            var failed = new StringIsValidPhoneNumberWithCountryCodeOrDefault ().ValidationFailed (phoneNumber);
+            Assert.False (failed);
+        }
+    }
+}
