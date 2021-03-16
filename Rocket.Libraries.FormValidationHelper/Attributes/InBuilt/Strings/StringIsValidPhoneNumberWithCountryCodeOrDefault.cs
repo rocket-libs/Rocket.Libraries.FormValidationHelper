d77d4dee53fd1f30d09282e6f5473b0781cbec75
nyingi <nyingi.maina@gmail.com>
2021-03-16 20:22:00 +0300

    Added option for phone number validation to be done without the leading '+' sign

--- Rocket.Libraries.FormValidationHelper/Attributes/InBuilt/Strings/StringIsValidPhoneNumberWithCountryCodeOrDefault.cs
+++ Rocket.Libraries.FormValidationHelper/Attributes/InBuilt/Strings/StringIsValidPhoneNumberWithCountryCodeOrDefault.cs
@@ -2,14 +2,25 @@ namespace Rocket.Libraries.FormValidationHelper.Attributes.InBuilt.Strings
 {
     public class StringIsValidPhoneNumberWithCountryCodeOrDefault : ValidatorAttributeBase
     {
+        private readonly bool requireLeadingPlusSign;
         private string valueAsString;
 
-        public StringIsValidPhoneNumberWithCountryCodeOrDefault(string displayLabel)
-         : base(displayLabel)
+        /// <param name="displayLabel">A user friendly name of the field being validated.</param>
+        /// <param name="requireLeadingPlusSign">
+        /// If phone numbers must start with the 'plus' sign, set this to true.
+        /// Set it to false, if it is ok for phone numbers to start with just the country code
+        /// without the leading 'plus' sign.
+        /// </param>
+        /// <returns></returns>
+        public StringIsValidPhoneNumberWithCountryCodeOrDefault (
+            string displayLabel,
+            bool requireLeadingPlusSign)
+             : base (displayLabel)
         {
+            this.requireLeadingPlusSign = requireLeadingPlusSign;
         }
 
-        public override string ErrorMessage => InsertDisplayLabel($"Value '{valueAsString}' for {ValidatorAttributeBase.DisplayLabelPlaceholder} is not a valid phone number. A leading '+' symbol as well as country code are expected");
+        public override string ErrorMessage => InsertDisplayLabel ($"Value '{valueAsString}' for {ValidatorAttributeBase.DisplayLabelPlaceholder} is not a valid phone number. A leading '+' symbol as well as country code are expected");
 
         public override bool ValidationFailed (object value)
         {
@@ -20,6 +31,14 @@ namespace Rocket.Libraries.FormValidationHelper.Attributes.InBuilt.Strings
             else
             {
                 valueAsString = value.ToString ();
+                if (requireLeadingPlusSign == false && !valueAsString.StartsWith ("+"))
+                {
+                    // 'Plus' sign is always required, so the 'requireLeadingPlusSign' flag in the constructor
+                    // is simply for user convenience, and we'll always add the sign in.
+                    // Strings are immutable, this won't change the original value on the user's object
+                    // but it will allow us perform the validation.
+                    valueAsString = $"+{valueAsString}";
+                }
             }
             if (string.IsNullOrEmpty (valueAsString))
             {
