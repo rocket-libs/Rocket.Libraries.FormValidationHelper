f3dd892222bde688bc46fa2cc935de12f0251794
nyingi <nyingi.maina@gmail.com>
2021-07-28 09:23:26 +0300

    Added functionality to test that a number is not in a given set of numbers

---
+++ Rocket.Libraries.FormValidationHelper/Attributes/InBuilt/Numbers/NumberIsNotInSet.cs
@@ -0,0 +1,47 @@
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Rocket.Libraries.FormValidationHelper.Attributes.InBuilt.Numbers
+{
+    public class NumberIsNotInSet : ValidatorAttributeBase
+    {
+        private readonly ImmutableHashSet<double> restrictedSet;
+        private string stringValue;
+
+        private double numberValue;
+        public NumberIsNotInSet (string displayLabel, params double[] set) : base (displayLabel)
+        {
+            if (set == null)
+            {
+                restrictedSet = ImmutableHashSet<double>.Empty;
+            }
+            else
+            {
+                restrictedSet = set.GroupBy (a => a).Select (a => a.Key).ToImmutableHashSet ();
+            }
+        }
+
+        public override string ErrorMessage => InsertDisplayLabel ($"The value '{stringValue}' is not allowed for field {ValidatorAttributeBase.DisplayLabelPlaceholder}.");
+        
+        public override bool ValidationFailed (object value)
+        {
+            if (value == default)
+            {
+                stringValue = "0";
+            }
+            else
+            {
+                stringValue = value.ToString ();
+            }
+
+            if (double.TryParse (stringValue, out numberValue) == false)
+            {
+                return true;
+            }
+            else
+            {
+                return restrictedSet.Any (restrictedNumber => restrictedNumber == numberValue);
+            }
+        }
+    }
+}
